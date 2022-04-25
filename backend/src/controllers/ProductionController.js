4a827899790305fad5e66d77df3c21e481ead55c
rafael-14 <fcramos12@hotmail.com>
2022-04-25 11:02:16 -0300

    showin next processes correctly

--- backend/src/controllers/ProductionController.js
+++ backend/src/controllers/ProductionController.js
@@ -4,19 +4,18 @@ const nextProcess = require('../functions/nextProcess')
 module.exports = {
 
   async selectProductionNotStarted(req, res) {
-    await connectionPG.query(`select prodc.*, prod.nome as nome_produto, proc.nome as nome_processo, proc_prod.sequencia, prod_ped.observacao
+    await connectionPG.query(`select prodc.*, prod.nome as nome_produto, proc.nome as nome_processo, prod_ped.observacao
     from producao prodc
     join produto prod on prod.id = prodc.id_produto
     join processo proc on proc.id = prodc.id_processo
-    join processos_por_produto proc_prod on proc_prod.id_produto = prodc.id_produto
+    --join processos_por_produto proc_prod on proc_prod.id_produto = prodc.id_produto
     join produtos_por_pedido prod_ped on prod_ped.id_pedido = prodc.id_pedido and prod_ped.id_produto = prodc.id_produto
-    where prodc.situacao = 0
-    and proc_prod.sequencia = 1`)
+    where prodc.situacao = 0`)
       .then(results => { productionNotStarted = results.rows })
     for (let i = 0; i < productionNotStarted.length; i++) {
-      let dataNextProcess = await nextProcess.nextProcess(productionNotStarted[i].id_produto, productionNotStarted[i].sequencia)
-      productionNotStarted[i].nome_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].nome_proximo_processo)
-      productionNotStarted[i].id_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].id_proximo_processo)
+      let dataNextProcess = await nextProcess.nextProcess(productionNotStarted[i].id_produto, productionNotStarted[i].id_processo)
+      productionNotStarted[i].nome_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].nome)
+      productionNotStarted[i].id_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].id)
     }
     return res.json(productionNotStarted)
   },
@@ -32,10 +31,9 @@ module.exports = {
     and proc_prod.sequencia = 1`)
       .then(results => { productionStarted = results.rows })
     for (let i = 0; i < productionStarted.length; i++) {
-      let dataNextProcess = await nextProcess.nextProcess(productionStarted[i].id_produto, productionStarted[i].sequencia)
-      productionStarted[i].nome_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].nome_proximo_processo)
-      productionStarted[i].id_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].id_proximo_processo)
-
+      let dataNextProcess = await nextProcess.nextProcess(productionStarted[i].id_produto, productionStarted[i].id_processo)
+      productionStarted[i].nome_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].nome)
+      productionStarted[i].id_proximo_processo = (dataNextProcess[0] === undefined ? null : dataNextProcess[0].id)
     }
     return res.json(productionStarted)
   },
--- backend/src/functions/nextProcess.js
+++ backend/src/functions/nextProcess.js
@@ -1,13 +1,20 @@
 const connectionPG = require('../database/');
 
 module.exports = {
-    async nextProcess(productID, processByProduct_Sequence) {
-        await connectionPG.query(`select proc_prod.*, proc.nome as nome_proximo_processo, proc.id as id_proximo_processo 
-            from processos_por_produto proc_prod
+    async nextProcess(productID, processID) {
+        await connectionPG.query(`select * from processos_por_produto proc_prod
             join processo proc on proc.id = proc_prod.id_processo
             where id_produto = ${productID}
-            and sequencia = ${processByProduct_Sequence + 1}`)
+            and id_processo = ${processID}`)
             .then(results => { nextProcess = results.rows })
+        for (let i = 0; i < nextProcess.length; i++) {
+            await connectionPG.query(`select * from processos_por_produto proc_prod
+                join processo proc on proc.id = proc_prod.id_processo
+                where id_produto = ${productID}
+                and sequencia = ${nextProcess[i].sequencia + 1}`)
+                .then(results => { nextProcess = results.rows })
+        }
+        
         return nextProcess
     }
 }
