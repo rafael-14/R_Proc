b542c18876e456cec1c8eb3767caf28a3864be45
rafael-14 <fcramos12@hotmail.com>
2022-07-13 13:31:38 -0300

    adjust users list in new sectors page

--- frontend/src/pages/cadastrar/setores.js
+++ frontend/src/pages/cadastrar/setores.js
@@ -3,10 +3,11 @@ import api from '../../services/api';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {
-  Button, Switch, FormGroup, FormControlLabel, TableCell, TableBody, Chip,
-  Container, Grid, Paper, Box, TextField, Toolbar, Table, TableContainer, TableHead, TableRow,
-  Checkbox, CircularProgress
+  Button, Switch, FormControlLabel, TableCell, TableBody, Chip,
+  Container, Grid, Box, TextField, Toolbar, Table, TableContainer, TableHead, TableRow,
+  Checkbox, CircularProgress, Pagination
 } from "@mui/material";
+import Link from "next/link";
 
 export default function CadastrarSetores() {
 
@@ -69,14 +70,17 @@ export default function CadastrarSetores() {
     }
   }
 
+  let [page, setPage] = useState(1)
+  let [count, setCount] = useState(0)
   let [users, setUsers] = useState([])
   useEffect(() => {
     async function loadUsers() {
-      let response = await api.get('/api/select/users')
-      setUsers(response.data)
+      let response = await api.post('/api/select/users', { page })
+      setUsers(response.data.allUsers)
+      setCount(response.data.count)
     }
     loadUsers()
-  }, [])
+  }, [page])
 
   let [vinculatedUser, setVinculatedUser] = useState([])
   function handleVinculatedUser(id) {
@@ -101,94 +105,101 @@ export default function CadastrarSetores() {
     >
       <Toolbar />
       <ToastContainer />
-      <Container maxWidth="lg" sx={{ mt: 2 }}>
-        <Paper sx={{ p: 1, display: 'flex', flexDirection: 'column' }}>
-          <Grid >
-            <Grid container spacing={3}>
-              <Grid item xs={12} >
-                <TextField
-                  id="sector"
-                  fullWidth
-                  required
-                  label="Setor"
-                  color="secondary"
-                  value={sector}
-                  onChange={e => setSector(e.target.value)}
-                />
-              </Grid>
-            </Grid>
-            <Grid container>
-              <FormGroup>
-                <FormControlLabel
-                  control={<Switch
-                    checked={manyRegisters}
-                    onChange={() => setManyRegisters(!manyRegisters)}
-                  />}
-                  label="Cadastrar Vários"
-                />
-              </FormGroup>
-            </Grid>
-            <br />
-            <Grid item xs={5} >
-              <TableContainer >
-                <Table size="medium" stickyHeader>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell style={{ background: '#E8927C', color: '#FFFFFF' }} align="left">Usuários</TableCell>
-                      <TableCell style={{ background: '#E8927C', color: '#FFFFFF' }} align="right">Situação</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {users.map((row) => (
-                      <TableRow key={row.id}>
-                        <TableCell align="left">
-                          <Checkbox
-                            color="secondary"
-                            onClick={() => handleVinculatedUser(row.id)}
-                            disabled={row.ativo ? false : true}
-                          />
-                          {row.nome} {row.sobrenome}
-                        </TableCell>
-                        <TableCell align="right">
-                          <Chip size="small" label={row.ativo ? "Ativo" : "Inativo"} color={row.ativo ? "success" : "error"} />
-                        </TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Grid>
-            <Grid
-              container
-              direction="row"
-              justifyContent="flex-end"
-            >
-              {!progress ?
-                (<>
+      <Container maxWidth="xg" sx={{ mt: 2 }}>
+        <Grid >
+          <TextField
+            id="sector"
+            fullWidth
+            required
+            label="Setor"
+            color="secondary"
+            value={sector}
+            onChange={e => setSector(e.target.value)}
+          />
+        </Grid>
+        <FormControlLabel
+          control={<Switch
+            checked={manyRegisters}
+            onChange={() => setManyRegisters(!manyRegisters)}
+          />}
+          label="Cadastrar Vários"
+        />
+        <br />
+        <TableContainer >
+          <Table size="medium" stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell style={{ background: '#E8927C', color: '#FFFFFF' }} align="left">Usuários</TableCell>
+                <TableCell style={{ background: '#E8927C', color: '#FFFFFF' }} align="right">Situação</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {users.map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell align="left">
+                    <Checkbox
+                      color="secondary"
+                      onClick={() => handleVinculatedUser(row.id)}
+                      disabled={row.ativo ? false : true}
+                    />
+                    {row.nome} {row.sobrenome}
+                  </TableCell>
+                  <TableCell align="right">
+                    <Chip size="small" label={row.ativo ? "Ativo" : "Inativo"} color={row.ativo ? "success" : "error"} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <br />
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+        >
+          <div/>
+          <Grid item justifyContent="center">
+            {count > 10 ?
+              <Pagination
+                size="large"
+                showFirstButton
+                showLastButton
+                onChange={(_event, page) => setPage(page)}
+                count={(parseInt(count / 10) + (count % 10 !== 0 ? 1 : 0))}
+                defaultPage={1}
+                siblingCount={0}
+                page={page}
+              />
+              : null}
+          </Grid>
+          <Grid item>
+            {!progress ?
+              (<>
+                <Link href="/listar/setores">
                   <Button
                     variant="contained"
                     style={{
                       background: '#E74C3C',
                       color: "#FFFFFF"
                     }}
-                    href="/listar/setores"
                   >
                     Cancelar
                   </Button>
-                  <Button
-                    variant="contained"
-                    style={{ color: "#FFFFFF", marginInlineStart: 15, backgroundColor: "#E8927C" }}
-                    onClick={() => handleNewSector()}
-                  >
-                    Salvar
-                  </Button>
-                </>) :
-                (<Box sx={{ display: 'flex' }}>
-                  <CircularProgress />
-                </Box>)}
-            </Grid>
+                </Link>
+                <Button
+                  variant="contained"
+                  style={{ color: "#FFFFFF", marginInlineStart: 15, backgroundColor: "#E8927C" }}
+                  onClick={() => handleNewSector()}
+                >
+                  Salvar
+                </Button>
+              </>) :
+              (<Box sx={{ display: 'flex' }}>
+                <CircularProgress />
+              </Box>)}
           </Grid>
-        </Paper>
+        </Grid>
       </Container>
     </Box>
   );
